4d47b9747adbe5007b55c4fce63b4f52ec5b3fae
Noah Gentile <noah.gentile@gmail.com>
2019-09-22 14:34:44 -0400

    Scaffolding app

--- src/router.js
+++ src/router.js
@@ -1,25 +1,24 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+import Home from '@/views/Home';
 
-export default new Router({
+Vue.use(Router);
+
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
-})
+      path: '*',
+      component: () =>
+        import(/* webpackChunkName: "not-found" */ '@/views/NotFound'),
+    },
+  ],
+});
+
+export default router;
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+        data: '@import "src/assets/styles";',
+      },
+    },
+  },
+};
